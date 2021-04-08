95aa0a96b41a49a64f8fc47e49974daca9fbea19
Andrei Navoichyk <andrei@cere.io>
2021-04-08 13:14:17 +0300

    https should be used

--- scripts/insert-keys.sh
+++ scripts/insert-keys.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-NODE_0_URL=http://1.2.3.4:9933
-NODE_1_URL=http://5.6.7.8:9933
+NODE_0_URL=https://1.2.3.4:9934
+NODE_1_URL=https://5.6.7.8:9934
 
 curl ${NODE_0_URL} -H "Content-Type:application/json;charset=utf-8" -d "@scripts/keys/node_0_stash_gran.json"
 curl ${NODE_0_URL} -H "Content-Type:application/json;charset=utf-8" -d "@scripts/keys/node_0_gran.json"
