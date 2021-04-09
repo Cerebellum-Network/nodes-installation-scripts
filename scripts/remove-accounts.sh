c958f4c9721ce7e8c17b7823b8708e326bd506d0
Andrei Navoichyk <andrei@cere.io>
2021-04-09 10:57:50 +0300

    updated remove-accounts.sh

--- scripts/remove-accounts.sh
+++ scripts/remove-accounts.sh
@@ -2,11 +2,10 @@
 
 ### remove account data related to generate-accounts and validator
 
-rm -rf scripts/generate-accounts/accounts
+rm -rf accounts
 rm -rf scripts/keys
-sed -i '/PROVIDER/d' scripts/validator/.env
-sed -i "1s/^/PROVIDER=ws:\/\/add_validation_node_custom:9944\n/" scripts/validator/.env
-sed -i '/STASH_ACCOUNT_MNEMONIC/d' scripts/validator/.env
-sed -i "2s|^|STASH_ACCOUNT_MNEMONIC=STASH_ACCOUNT_MNEMONIC\n|" scripts/validator/.env
-sed -i '/CONTROLLER_ACCOUNT_MNEMONIC/d' scripts/validator/.env
-sed -i "3s|^|CONTROLLER_ACCOUNT_MNEMONIC=CONTROLLER_ACCOUNT_MNEMONIC\n|" scripts/validator/.env
+
+sed -i "" "s|PROVIDER=.*|PROVIDER=ws://add_validation_node_custom:9944|" scripts/validator/.env
+sed -i "" "s|STASH_ACCOUNT_MNEMONIC=.*|STASH_ACCOUNT_MNEMONIC=STASH_ACCOUNT_MNEMONIC|" scripts/validator/.env
+sed -i "" "s|CONTROLLER_ACCOUNT_MNEMONIC=.*|CONTROLLER_ACCOUNT_MNEMONIC=CONTROLLER_ACCOUNT_MNEMONIC|" scripts/validator/.env
+
