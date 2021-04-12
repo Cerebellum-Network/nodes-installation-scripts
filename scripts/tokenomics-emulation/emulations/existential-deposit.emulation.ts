ad23cb2f44b79744f87e5446c25ed4eb6d4773ce
Karthikeyan <karthikeyan@cere.io>
2021-04-12 19:44:24 +0530

    removing unwanted codes

--- scripts/tokenomics-emulation/emulations/existential-deposit.emulation.ts
+++ scripts/tokenomics-emulation/emulations/existential-deposit.emulation.ts
@@ -11,19 +11,16 @@ class ExistentialDepositEmulation implements IEmulation {
 
   public async run(): Promise<void> {
     for (let i = 1; i <= this.config.amount; i++) {
-      console.log(this.config);
       console.log(`Running ${i} existential deposit...\n`);
       const destination = await this.account.generateSrAccount();
       const sender = this.account.sudoAccount;
       const transferAmount = this.network.existentialDeposit();
-      console.log(sender.address);
-      console.log(transferAmount);
       const transfer = await this.network.transfer(
         sender,
-        '5EHG8yMxHBLPeK2Uyw9cnrdC3RY2yCa8qkwvHS7PLazErXfE',
+        destination.ss58Address,
         transferAmount.toString()
       );
-      const balance = await this.network.getBalance('5EHG8yMxHBLPeK2Uyw9cnrdC3RY2yCa8qkwvHS7PLazErXfE');
+      const balance = await this.network.getBalance(destination.ss58Address);
       console.log(`Balance of ${destination.ss58Address} is ${balance}`);
     }
   }
