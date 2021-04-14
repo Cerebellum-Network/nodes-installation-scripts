f2451baee3c3517f774c764a53eec53ce3c897c1
Andrei Navoichyk <andrei@cere.io>
2021-04-14 16:07:57 +0300

    added script to up boot node

--- scripts/tokenomics-emulation/run-boot.sh
+++ scripts/tokenomics-emulation/run-boot.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/Cerebellum-Network/nodes-installation-scripts.git cere-network
-cd cere-network
-git checkout feature/run-test
 chmod -R 777 chain-data
 docker-compose --env-file ./configs/.env.testnet up -d boot_node
 docker-compose logs -f | grep "Local node identity is"
