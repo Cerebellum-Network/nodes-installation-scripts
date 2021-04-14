c307a10721b3912591c437c34caf65f309612261
Andrei Navoichyk <andrei@cere.io>
2021-04-14 22:50:57 +0300

    start-boot script is added

---
+++ scripts/start-boot.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+ssh testnet-node-1-dev1 'git clone https://github.com/Cerebellum-Network/nodes-installation-scripts.git cere-network'
+ssh testnet-node-1-dev1 'cd cere-network && git checkout feature/run-2 && chmod -R 777 chain-data && docker-compose --env-file ./configs/.env.testnet up -d boot_node && docker-compose logs -f | grep "Local node identity is"'
