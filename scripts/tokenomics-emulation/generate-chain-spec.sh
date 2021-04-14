bd5082dbe7e93741e50e37f936d678d928f34708
Andrei Navoichyk <andrei@cere.io>
2021-04-14 16:04:41 +0300

    updated script

--- scripts/tokenomics-emulation/generate-chain-spec.sh
+++ scripts/tokenomics-emulation/generate-chain-spec.sh
@@ -4,6 +4,8 @@ rm -rf spec-data/customSpec.json
 rm -rf spec-data/customSpecRaw.json
 
 docker-compose up create_chain_spec
+docker-compose build generate_accounts
 docker-compose up generate_accounts
+docker-compose build generate_emulations_chain_spec
 docker-compose up generate_emulations_chain_spec
 docker-compose up create_raw_chain_spec
