0bcd218fff429b02ab3cbc458558dca737ef74ff
Andrei Navoichyk <andrei@cere.io>
2021-04-14 23:41:41 +0300

    updated start validators script

--- scripts/start-validators.sh
+++ scripts/start-validators.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-ssh testnet-node-2-dev1 'git clone https://github.com/Cerebellum-Network/nodes-installation-scripts.git cere-network'
-ssh testnet-node-2-dev1 'cd cere-network && git checkout feature/run-2 && chmod -R 777 chain-data && docker-compose --env-file ./configs/.env.testnet up -d add_validation_node_custom && docker-compose logs -f | grep "Local node identity is"'
+for i in 2 3 4 5 6 7
+do
+  ssh "testnet-node-${i}-dev1" 'git clone https://github.com/Cerebellum-Network/nodes-installation-scripts.git cere-network'
+  ssh "testnet-node-${i}-dev1" 'cd cere-network && git checkout feature/run-2 && chmod -R 777 chain-data && docker-compose --env-file ./configs/.env.testnet up -d add_validation_node_custom'
+done
