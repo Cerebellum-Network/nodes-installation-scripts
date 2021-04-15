2f037e1d42719c23253ed9dbd0ff315bbd3b7fa0
Andrei Navoichyk <andrei@cere.io>
2021-04-15 23:56:02 +0300

    update branch name

--- scripts/start-validators.sh
+++ scripts/start-validators.sh
@@ -3,5 +3,5 @@
 for i in 2 3 4 5 6 7
 do
   ssh "testnet-node-${i}-dev1" 'git clone https://github.com/Cerebellum-Network/nodes-installation-scripts.git cere-network'
-  ssh "testnet-node-${i}-dev1" 'cd cere-network && git checkout feature/run-2 && chmod -R 777 chain-data && docker-compose --env-file ./configs/.env.testnet up -d add_validation_node_custom'
+  ssh "testnet-node-${i}-dev1" 'cd cere-network && git checkout feature/run-3 && chmod -R 777 chain-data && docker-compose --env-file ./configs/.env.testnet up -d add_validation_node_custom'
 done
