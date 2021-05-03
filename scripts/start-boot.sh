42e23714df177e67448daccf29d0b315a3af7e13
Andrei Navoichyk <andrei@cere.io>
2021-05-03 18:44:23 +0300

    updated branch

--- scripts/start-boot.sh
+++ scripts/start-boot.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 ssh testnet-node-1-dev1 'sudo su -c "cd ../../root; git clone https://github.com/Cerebellum-Network/nodes-installation-scripts.git cere-network"'
-ssh testnet-node-1-dev1 'sudo su -c "cd ../../root/cere-network; git checkout feature/run-6; chmod -R 777 chain-data; docker-compose --env-file ./configs/.env.testnet up -d boot_node"'
+ssh testnet-node-1-dev1 'sudo su -c "cd ../../root/cere-network; git checkout feature/run-7; chmod -R 777 chain-data; docker-compose --env-file ./configs/.env.testnet up -d boot_node"'
