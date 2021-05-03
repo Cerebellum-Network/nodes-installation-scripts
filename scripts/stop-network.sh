001705184572a31b2fda6bcae82847c5a9467f78
Andrei Navoichyk <andrei@cere.io>
2021-05-03 18:25:48 +0300

    updated scripts

--- scripts/stop-network.sh
+++ scripts/stop-network.sh
@@ -3,8 +3,8 @@
 for i in 1 2 3 4 5 6 7
 do
   echo "Stopping ${i}"
-  ssh "testnet-node-${i}-dev1" "cd cere-network && docker-compose down"
-  ssh "testnet-node-${i}-dev1" "rm -rf cere-network"
+  ssh "testnet-node-${i}-dev1" 'sudo su -c "cd ../../root/cere-network; docker-compose down;"'
+  ssh "testnet-node-${i}-dev1" 'sudo su -c "cd ../../root; rm -rf cere-network;"'
 done
 
 
