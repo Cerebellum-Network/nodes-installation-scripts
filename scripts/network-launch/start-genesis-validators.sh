58c20306bf6e2921cc7f4e471d20f2f3228c3e26
Andrei Navoichyk <andrei@cere.io>
2021-05-15 22:07:47 +0300

    added network stop/launch scripts

---
+++ scripts/network-launch/start-genesis-validators.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+ssh testnet-node-1-dev1 'sudo su -c "cd ../../root; git clone https://github.com/Cerebellum-Network/nodes-installation-scripts.git cere-network"'
+ssh testnet-node-1-dev1 'sudo su -c "cd ../../root/cere-network; git checkout feature/launch; chmod -R 777 chain-data; docker-compose --env-file ./configs/.env.testnet up -d boot_node"'
