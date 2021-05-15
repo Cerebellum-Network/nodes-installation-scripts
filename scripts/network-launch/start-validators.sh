80c8034d2f2b8e057127718ff49fd64f1e97cd13
Andrei Navoichyk <andrei@cere.io>
2021-05-16 00:00:46 +0300

    added start validators script

---
+++ scripts/network-launch/start-validators.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+for i in 2 3 4 5 6 7
+do
+  ssh "testnet-node-${i}-dev1" 'sudo su -c "cd ../../root; git clone https://github.com/Cerebellum-Network/nodes-installation-scripts.git cere-network"'
+  ssh "testnet-node-${i}-dev1" 'sudo su -c "cd ../../root/cere-network; git checkout feature/launch; chmod -R 777 chain-data; docker-compose --env-file ./configs/.env.testnet up -d add_validation_node_custom"'
+done
