edfaad45ad783e83cb04264e639f42f1013952e0
Andrei Navoichyk <andrei@cere.io>
2021-05-16 00:07:54 +0300

    added IPs

--- scripts/network-launch/insert-keys.sh
+++ scripts/network-launch/insert-keys.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-NODE_0_URL=http://1.2.3.4:9933
-NODE_1_URL=http://5.6.7.8:9933
+NODE_0_URL=http://164.90.155.170:9933
+NODE_1_URL=http://104.236.193.202:9933
 
 curl ${NODE_0_URL} -H "Content-Type:application/json;charset=utf-8" -d "@scripts/keys/node_0_stash_gran.json"
 curl ${NODE_0_URL} -H "Content-Type:application/json;charset=utf-8" -d "@scripts/keys/node_0_gran.json"
