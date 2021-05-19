98f2bd46f595367f33fc4929f5a8ff353652fbfd
Andrei Navoichyk <andrei@cere.io>
2021-05-20 02:33:53 +0300

    key should be take from file

--- scripts/tokenomics-emulation/generate-chain-spec.ts
+++ scripts/tokenomics-emulation/generate-chain-spec.ts
@@ -85,7 +85,8 @@ class ChainSpecGenerator {
 
         const rootAccountBalance = config.network.total_supply - aliceBalance - totalGenesisValidatorsStake - totalValidatorsStake - totalNominatorsStake - totalGenesisCouncilsStake - treasuryStake - sudoStake;
         if (config.network.manual_bridge) {
-            spec.genesis.runtime.palletBalances.balances.push([config.network.manual_bridge.address, (10 ** spec.properties.tokenDecimals) * rootAccountBalance]);
+            const manualBridgeAccount = this.readAccount(`manual-bridge`);
+            spec.genesis.runtime.palletBalances.balances.push([manualBridgeAccount.ss58Address, (10 ** spec.properties.tokenDecimals) * rootAccountBalance]);
         } else {
             spec.genesis.runtime.palletBalances.balances.push([rootAccount.ss58Address, (10 ** spec.properties.tokenDecimals) * rootAccountBalance]);
         }
