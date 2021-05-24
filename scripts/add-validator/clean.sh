7bb34fc853ef1f25e28a14d15fa149aa280f70da
Karthikeyan <karthikeyan@cere.io>
2021-05-24 13:20:51 +0530

    moved remove accounts to add-validator and generate-accounts

---
+++ scripts/add-validator/clean.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+### validator
+
+sed -i "" "s|PROVIDER=.*|PROVIDER=ws://add_validation_node_custom:9944|" scripts/validator/.env
+sed -i "" "s|STASH_ACCOUNT_MNEMONIC=.*|STASH_ACCOUNT_MNEMONIC=STASH_ACCOUNT_MNEMONIC|" scripts/validator/.env
+sed -i "" "s|CONTROLLER_ACCOUNT_MNEMONIC=.*|CONTROLLER_ACCOUNT_MNEMONIC=CONTROLLER_ACCOUNT_MNEMONIC|" scripts/validator/.env
---
+++ scripts/generate-accounts/clean.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+### remove account data related to generate-accounts
+
+rm -rf accounts
+rm -rf scripts/keys
