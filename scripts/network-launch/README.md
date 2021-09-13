8bf3f6c83ef949fff64dfe75c48beea03d594bba
Karthikeyan <karthikeyan@cere.io>
2021-09-13 20:24:24 +0530

    seperated cluster config from launch script

--- scripts/network-launch/README.md
+++ scripts/network-launch/README.md
@@ -1,3 +1,9 @@
 # Launch Network
 
 Scripts to launch a Network.
+
+```bash
+$ bash launch.sh --file=<CLUSTER-CONFIG-PATH>
+```
+
+CLUSTER-CONFIG-PATH --> cluster config file path
--- scripts/network-launch/launch.sh
+++ scripts/network-launch/launch.sh
@@ -1,145 +1,5 @@
 #!/usr/bin/env bash
 
-#### DEV ####
-#bootNodeIP=164.90.173.129
-#bootNodeHost=testnet-node-1.dev.cere.network
-#genesisValidatorIP=138.68.136.162
-#genesisValidatorHost=testnet-node-2.dev.cere.network
-#validatorsIPs=(165.227.57.153)
-#validatorsHosts=(testnet-node-3.dev.cere.network)
-#fullNodeIP=167.71.6.56
-#fullNodeHost=rpc.testnet.dev.cere.network
-#archiveNodeIP=128.199.37.230
-#archiveNodeHost=archive.testnet.dev.cere.network
-#user="andrei"
-#path="../../root/"
-
-#### DEV1 ####
-#bootNodeIP=164.90.155.170
-#bootNodeHost=testnet-node-1.dev1.cere.network
-#genesisValidatorIP=104.236.193.202
-#genesisValidatorHost=testnet-node-2.dev1.cere.network
-#validatorsIPs=(167.99.188.91\
-#               167.99.131.218\
-#               165.227.224.150)
-#validatorsHosts=(testnet-node-3.dev1.cere.network\
-#                 testnet-node-4.dev1.cere.network\
-#                 testnet-node-5.dev1.cere.network)
-#fullNodeIP=138.197.202.96
-#fullNodeHost=testnet-node-6.dev1.cere.network
-#archiveNodeIP=134.209.192.121
-#archiveNodeHost=testnet-node-7.dev1.cere.network
-#user="andrei"
-#path="../../root/"
-
-### QANET ####
-#bootNodeIP=161.35.229.40
-#bootNodeHost=node-1.qanet.cere.network
-#genesisValidatorIP=159.203.161.219
-#genesisValidatorHost=node-2.qanet.cere.network
-#validatorsIPs=(134.122.45.69\
-#               64.225.98.192\
-#               138.68.132.196)
-#validatorsHosts=(node-3.qanet.cere.network\
-#                 node-4.qanet.cere.network\
-#                 node-5.qanet.cere.network)
-#fullNodeIP=143.198.66.9
-#fullNodeHost=rpc.qanet.cere.network
-#archiveNodeIP=104.248.91.35
-#archiveNodeHost=archive.qanet.cere.network
-#user="andrei"
-#path="../../root/"
-
-### TESTNET ####
-#bootNodeIP=165.232.149.206
-#bootNodeHost=node-1.testnet.cere.network
-#genesisValidatorIP=45.55.62.114
-#genesisValidatorHost=node-2.testnet.cere.network
-#validatorsIPs=(159.203.13.16\
-#               157.230.106.171\
-#               159.65.26.196\
-#               138.68.20.192)
-#validatorsHosts=(node-3.testnet.cere.network\
-#                 node-4.testnet.cere.network\
-#                 node-5.testnet.cere.network\
-#                 node-6.testnet.cere.network)
-#fullNodeIP=143.198.130.232
-#fullNodeHost=rpc.testnet.cere.network
-#archiveNodeIP=178.128.250.106
-#archiveNodeHost=archive.testnet.cere.network
-#user="andrei"
-#path="../../root/"
-
-### EXT DEVS CLUSTER 1 ###
-#bootNodeIP=143.198.145.104
-#bootNodeHost=ext-devs-node-1.cluster-1.cere.network
-#genesisValidatorIP=161.35.140.182
-#genesisValidatorHost=ext-devs-node-2.cluster-1.cere.network
-#validatorsIPs=(159.203.31.45)
-#validatorsHosts=(ext-devs-node-3.cluster-1.cere.network)
-#fullNodeIP=""
-#fullNodeHost=""
-#archiveNodeIP=""
-#archiveNodeHost=""
-#user="andrei"
-#path="../../root/"
-
-### EXT DEVS CLUSTER 2 ###
-#bootNodeIP=144.126.223.105
-#bootNodeHost=ext-devs-node-1.cluster-2.cere.network
-#genesisValidatorIP=164.90.136.15
-#genesisValidatorHost=ext-devs-node-2.cluster-2.cere.network
-#validatorsIPs=(143.110.214.208)
-#validatorsHosts=(ext-devs-node-3.cluster-2.cere.network)
-#fullNodeIP=""
-#fullNodeHost=""
-#archiveNodeIP=""
-#archiveNodeHost=""
-#user="andrei"
-#path="../../root/"
-
-### EXT DEVS CLUSTER 3 ###
-#bootNodeIP=144.126.220.75
-#bootNodeHost=ext-devs-node-1.cluster-3.cere.network
-#genesisValidatorIP=64.225.49.33
-#genesisValidatorHost=ext-devs-node-2.cluster-3.cere.network
-#validatorsIPs=(142.93.150.99)
-#validatorsHosts=(ext-devs-node-3.cluster-3.cere.network)
-#fullNodeIP=""
-#fullNodeHost=""
-#archiveNodeIP=""
-#archiveNodeHost=""
-#user="root"
-#path="../../root/"
-
-### EXT DEVS CLUSTER 4 ###
-#bootNodeIP=143.110.234.113
-#bootNodeHost=ext-devs-node-1.cluster-4.cere.network
-#genesisValidatorIP=159.65.191.43
-#genesisValidatorHost=ext-devs-node-2.cluster-4.cere.network
-#validatorsIPs=(165.22.225.139)
-#validatorsHosts=(ext-devs-node-3.cluster-4.cere.network)
-#fullNodeIP=""
-#fullNodeHost=""
-#archiveNodeIP=""
-#archiveNodeHost=""
-#user="root"
-#path="../../root/"
-
-### EXT DEVS CLUSTER 5 ###
-#bootNodeIP=137.184.32.213
-#bootNodeHost=ext-devs-node-1.cluster-5.cere.network
-#genesisValidatorIP=138.197.97.48
-#genesisValidatorHost=ext-devs-node-2.cluster-5.cere.network
-#validatorsIPs=(143.198.37.140)
-#validatorsHosts=(ext-devs-node-3.cluster-5.cere.network)
-#fullNodeIP=""
-#fullNodeHost=""
-#archiveNodeIP=""
-#archiveNodeHost=""
-#user="root"
-#path="../../root/"
-
 repo=https://github.com/Cerebellum-Network/nodes-installation-scripts.git
 repoBranch="master"
 dirName="cere-network"
@@ -280,3 +140,16 @@ case $1 in
   stop_network) "$@"; exit;;
   remove_accounts) "$@"; exit;;
 esac
+
+for arg in "$@"
+do
+  case $arg in
+    --file=*)
+      path=`echo $arg | sed -e 's/^[^=]*=//g'`
+      echo "Loading cluster config from ${path}"
+      . $path
+      ;;
+    *)
+      echo "Unexpected option ${arg}" ;;
+  esac
+done
