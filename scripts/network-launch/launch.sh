7c29d4f468b640117718e9f19306d186fd90fd00
Andrei Navoichyk <andrei@cere.io>
2022-06-14 18:42:16 +0400

    fix typo

--- scripts/network-launch/launch.sh
+++ scripts/network-launch/launch.sh
@@ -43,8 +43,8 @@ start_validators () {
 insert_keys () {
 #  NODE_0_URL=https://${bootNodeHost}:9934
 #  NODE_1_URL=https://${genesisValidatorHost}:9934
-  NODE_0_URL=http://${bootNodeIp}:9933
-  NODE_1_URL=http://${genesisValidatorIp}:9933
+  NODE_0_URL=http://${bootNodeIP}:9933
+  NODE_1_URL=http://${genesisValidatorIP}:9933
 
   curl ${NODE_0_URL} -H "Content-Type:application/json;charset=utf-8" -d "@scripts/generate-accounts/keys/node_0_stash_gran.json"
   curl ${NODE_0_URL} -H "Content-Type:application/json;charset=utf-8" -d "@scripts/generate-accounts/keys/node_0_gran.json"
@@ -88,10 +88,10 @@ start_node () {
 #      bootNodeID=$(curl -H 'Content-Type: application/json' --data '{ "jsonrpc":"2.0", "method":"system_localPeerId", "id":1 }' https://${bootNodeHost}:9934 -s | jq '.result')
 #      sleep 5
 #  done
-  bootNodeID=$(curl -H 'Content-Type: application/json' --data '{ "jsonrpc":"2.0", "method":"system_localPeerId", "id":1 }' http://${bootNodeIp}:9933 -s | jq '.result')
+  bootNodeID=$(curl -H 'Content-Type: application/json' --data '{ "jsonrpc":"2.0", "method":"system_localPeerId", "id":1 }' http://${bootNodeIP}:9933 -s | jq '.result')
   while [ -z $bootNodeID ]; do
       echo "*** bootNodeID is empty "
-      bootNodeID=$(curl -H 'Content-Type: application/json' --data '{ "jsonrpc":"2.0", "method":"system_localPeerId", "id":1 }' http://${bootNodeIp}:9933 -s | jq '.result')
+      bootNodeID=$(curl -H 'Content-Type: application/json' --data '{ "jsonrpc":"2.0", "method":"system_localPeerId", "id":1 }' http://${bootNodeIP}:9933 -s | jq '.result')
       sleep 5
   done
   ssh ${user}@${ip} 'bash -s'  << EOT
